0716cff88080a7bbf1c7b625b609514faf3b4454
Martinique <me@martidolce.com>
2020-11-13 23:59:08 -0500

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,13 @@
-def my_each # put argument(s) here
-  # code here
+# frozen_string_literal: true
+
+# Author: Martinique Dolce
+# Course: Flatiron School 2020, November 9 - 20201, April 2021
+# Contact: me@martidolce.com | https://modis.martidolce.com
+def my_each(collection)
+  i = 0
+  while i < collection.length
+    yield(collection[i])
+    i += 1
+  end
+  collection
 end
